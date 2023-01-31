6244e644c304bff4a7cc62e260be2483e39c0470
Paata Home <paata.tepnadze@gmail.com>
2023-01-31 13:40:04 +0100

    add todoList component

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="App">
+    <h1>Todo Application</h1>
+    <hr />
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList: TodoList,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
